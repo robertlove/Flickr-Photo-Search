f0a00b280b2dab2760aebaa8606ea331bd9a37e7
Signified <robert.love@signified.com.au>
2011-07-17 21:17:57 +1000

    Making README worth reading

--- app/Config/database.php
+++ app/Config/database.php
@@ -3,6 +3,6 @@ class DATABASE_CONFIG
 {
     public $default = array(
         'datasource' => 'FlickrPhotoSource',
-        'key' => '8d2b2b0bcb73f2e3883b702a89b425a9'
+        'key' => 'your_flickr_api_key'
     );
 }
